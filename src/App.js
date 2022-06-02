2f7c10168cd3a962d2511d4fffc8bed36efd1b43
Harikrishnan <eatsndbites@gmail.com>
2022-06-02 20:39:53 +0530

    Console.log removed

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ const App = () => {
   const [otp, setOtp] = useState([]);
 
   const handleInputChange = (val, otpIndex) => {
-    console.log(val, otpIndex);
     let OTPCopy = [...otp];
-    console.log(OTPCopy[otpIndex]);
     if (OTPCopy[otpIndex] === undefined) {
       OTPCopy.push(val);
     } else {
       OTPCopy.forEach((input, index, array) => {
-        console.log(input, index, array);
         if (index === otpIndex) {
           array[otpIndex] = val;
         }
       });
     }
     setOtp(OTPCopy);
-    console.log(OTPCopy);
   };
 
   const handleClear = () => {
